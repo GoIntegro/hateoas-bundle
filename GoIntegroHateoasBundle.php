19dfdda20c2c3b28a07bf299ce9782e844caee18
Javier Lorenzana <javier.lorenzana@gointegro.com>
2014-10-27 15:34:14 -0300

    Adding the @author tag to file-level phpdocs. Egolatry.

--- GoIntegroHateoasBundle.php
+++ GoIntegroHateoasBundle.php
@@ -2,6 +2,7 @@
 /**
  * @copyright 2014 Integ S.A.
  * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ * @author Javier Lorenzana <javier.lorenzana@gointegro.com>
  */
 /**
  * @copyright 2014 Integ S.A.
--- Metadata/Entity/EntityMetadata.php
+++ Metadata/Entity/EntityMetadata.php
@@ -2,6 +2,7 @@
 /**
  * @copyright 2014 Integ S.A.
  * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ * @author Javier Lorenzana <javier.lorenzana@gointegro.com>
  */
 
 namespace GoIntegro\Bundle\HateoasBundle\Metadata\Entity;
