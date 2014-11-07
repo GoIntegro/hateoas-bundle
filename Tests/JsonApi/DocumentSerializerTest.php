17c47582b2948a5b61131ca33b01cc37ed1a2f12
Javier Lorenzana <javier.lorenzana@gointegro.com>
2014-11-07 16:28:19 -0300

    I renamed too much.

--- Tests/JsonApi/DocumentSerializerTest.php
+++ Tests/JsonApi/DocumentSerializerTest.php
@@ -18,7 +18,7 @@ class DocumentSerializerTest extends TestCase
 {
     const RESOURCE_ID = 'someId', RESOURCE_TYPE = 'resources';
 
-    public function testSerializingEmptyDocumentResource()
+    public function testSerializingEmptyResourceDocument()
     {
         /* Given... (Fixture) */
         $metadata = Stub::makeEmpty(
@@ -52,7 +52,7 @@ class DocumentSerializerTest extends TestCase
         $this->assertEquals(['resources' => NULL], $json);
     }
 
-    public function testSerializingIndividualDocumentResource()
+    public function testSerializingIndividualResourceDocument()
     {
         /* Given... (Fixture) */
         $metadata = Stub::makeEmpty(
