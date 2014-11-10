b21cecf669b32e6a449a10c6c99a8682c4f2993e
Javier Lorenzana <javier.lorenzana@gointegro.com>
2014-11-10 17:49:32 -0300

    Adding a service to find RAML documentation.

---
+++ Raml/DocFinder.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * @copyright 2014 Integ S.A.
+ * @license http://opensource.org/licenses/MIT The MIT License (MIT)
+ * @author Javier Lorenzana <javier.lorenzana@gointegro.com>
+ */
+
+namespace GoIntegro\Bundle\HateoasBundle\Raml;
+
+// YAML.
+use Symfony\Component\Yaml\Yaml;
+// HATEOAS.
+use GoIntegro\Bundle\HateoasBundle\JsonApi\ResourceEntityInterface;
+
+class DocFinder
+{
+    const ERROR_PARAM_TYPE = "A resource type or entity was expected.";
+
+    /**
+     * @var array
+     */
+    private $magicServices;
+
+    /**
+     * @param array $config
+     */
+    public function __construct(array $config = [])
+    {
+        // @todo Esta verificación debería estar en el DI.
+        $this->magicServices = isset($config['magic_services'])
+            ? $config['magic_services']
+            : [];
+    }
+
+    /**
+     * @param ResourceEntityInterface|string $clue A resource type or entity.
+     * @return array
+     * @throws \InvalidArgumentException
+     */
+    public function find($clue)
+    {
+        $filepath = NULL;
+
+        if (is_string($clue)) {
+            $filepath = $this->getRamlDocFromType($clue);
+        } elseif ($clue instanceof ResourceEntityInterface) {
+            $filepath = $this->getRamlDoc($clue);
+        } else {
+            throw new \InvalidArgumentException(self::ERROR_PARAM_TYPE);
+        }
+
+        return Yaml::parse($filepath);
+    }
+
+    /**
+     * @param string $type
+     * @return string
+     * @throws \RuntimeException
+     */
+    private function getRamlDocFromType($type)
+    {
+        foreach ($this->magicServices as $service) {
+            if (
+                $type === $service['resource_type']
+                && is_readable($service['raml_doc'])
+            ) {
+                return $service['raml_doc'];
+            }
+        }
+
+        throw new \RuntimeException(self::ERROR_UNKNOWN_TYPE);
+    }
+}
