0ff3d35d1ae5ca119594d643e6865e5d1c170a7c
Javier Lorenzana <javier.lorenzana@gointegro.com>
2014-11-11 17:26:14 -0300

    Switching the example app to the PHP bundled web server.

--- example/src/HateoasInc/Bundle/ExampleBundle/Controller/UsersController.php
+++ example/src/HateoasInc/Bundle/ExampleBundle/Controller/UsersController.php
@@ -6,56 +6,13 @@ namespace HateoasInc\Bundle\ExampleBundle\Controller;
 use GoIntegro\Bundle\HateoasBundle\Controller\Controller,
     Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 // Entidades.
-use GoIntegro\Entity\User;
+use HateoasInc\Bundle\ExampleBundle\Entity\User;
 
 /**
  * La API privada de usuarios - require autenticación.
  */
 class UsersController extends Controller
 {
-    /**
-     * @Route("/users/{user}", name="api_get_user", methods="GET")
-     */
-    public function getUserAction(User $user)
-    {
-        $resource = $this->get('hateoas.resource_manager')
-            ->createResourceFactory()
-            ->setEntity($user)
-            ->create();
-        $json = $this->get('hateoas.resource_manager')
-            ->createSerializerFactory()
-            ->setDocumentResources($resource)
-            ->addMeta(["hum"])
-            ->create()
-            ->serialize();
-
-        // Get the document as an array and output it as JSON
-        return $this->createETagResponse($json);
-    }
-
-    /**
-     * @Route("/users", name="api_get_users", methods="GET")
-     */
-    public function getUsersAction()
-    {
-        $users = $this->getDoctrine()
-            ->getManager()
-            ->getRepository('GoIntegro\Entity\User')
-            ->findByPlatform($this->getUser()->getPlatform());
-        $resources = $this->get('hateoas.resource_manager')
-            ->createCollectionFactory()
-            ->addEntities($users)
-            ->create();
-        $json = $this->get('hateoas.resource_manager')
-            ->createSerializerFactory()
-            ->setDocumentResources($resources)
-            ->create()
-            ->serialize();
-
-        // Get the document as an array and output it as JSON
-        return $this->createETagResponse($json);
-    }
-
     /**
      * @Route("/users", name="api_create_users", methods="POST")
      */
