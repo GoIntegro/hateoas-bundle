62053ca01ac7e97e708b34e7d8022daabdb711bb
Juan Jose Toledo <juanjose83218@gmail.com>
2015-01-19 10:18:40 -0300

    creando util para construir cliente http que consuma la api

---
+++ Util/HttpClientUtil.php
@@ -0,0 +1,66 @@
+<?php
+namespace GoIntegro\Bundle\HateoasBundle\Util;
+use GoIntegro\Hateoas\Http\Client;
+
+class HttpClientUtil
+{
+    const CONTENT_JSON_API = 'application/vnd.api+json',
+          HEADER_LOCALE = 'en-GB',
+          WSSE_CREDENTIALS_PATTERN = "X-WSSE: UsernameToken Username=\"%s\", PasswordDigest=\"%s\", Nonce=\"%s\", Created=\"%s\"";
+
+    /**
+     * Obtiene un cliente de HTTP.
+     * @param string $url
+     * @param string $username
+     * @param string $password
+     * @return Client
+     */
+    public static function buildHttpClient(
+        $url = NULL,
+        $username = NULL,
+        $password = NULL,
+        $contentType = self::CONTENT_JSON_API,
+        $language = self::HEADER_LOCALE
+    )
+    {
+        $client = new Client($url);
+        $client->setHead([
+            'Accept: ' . $contentType,
+            'Accept-Language: ' . $language,
+            'Content-Type: application/vnd.api+json'
+        ]);
+
+        if (is_scalar($username) && is_scalar($password)) {
+            $client->setOptions([CURLOPT_HTTPAUTH, CURLAUTH_ANY]);
+            $client->setOption(CURLOPT_USERPWD, $username . ':' . $password);
+        }
+
+        return $client;
+    }
+
+    /**
+     * Compone un header de autenticación WSSE.
+     * @param string $username
+     * @param string $password
+     * @see http://en.wikipedia.org/wiki/WS-Security
+     */
+    public static function buildWsseHeader($username, $password)
+    {
+        $nonce = mt_rand();
+        $date = new \DateTime();
+        $createdAt = $date->format("c");
+        $passwordDigest = base64_encode(sha1(
+            base64_decode($nonce) . $createdAt . $password,
+            TRUE
+        ));
+        $wsseCredentials = sprintf(
+            static::WSSE_CREDENTIALS_PATTERN,
+            $username,
+            $passwordDigest,
+            $nonce,
+            $createdAt
+        );
+
+        return $wsseCredentials;
+    }
+}
